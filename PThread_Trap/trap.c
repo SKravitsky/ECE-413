791daeb1822ee26e829ccd0687676295f3d9b03f
Kravitsky <sdk54@xunil-05.coe.drexel.edu>
2017-02-08 18:10:02 -0500

    Added timing code, cleaned up variable names

--- PThread_Trap/trap.c
+++ PThread_Trap/trap.c
@@ -26,31 +26,50 @@
 #define LEFT_ENDPOINT 5
 #define RIGHT_ENDPOINT 1000
 #define NUM_TRAPEZOIDS 100000000
-#define NUM_THREADS 4
+#define NUM_THREADS 16
 
 double compute_using_pthreads(float, float, int, float);
 double compute_gold(float, float, int, float);
 void pthread_compute(void* rank);
 
+int thread_n;
 double area = 0.0;
 pthread_mutex_t mutex;
-int local_n;
+
 
 
 int main(void) 
 {
+	struct timeval start1,stop1,start2,stop2;
+
 	int n = NUM_TRAPEZOIDS;
 	float a = LEFT_ENDPOINT;
 	float b = RIGHT_ENDPOINT;
 	float h = (b-a)/(float)n; // Height of each trapezoid  
 	printf("The height of the trapezoid is %f \n", h);
-
-	double reference = compute_gold(a, b, n, h);
-   printf("Reference solution computed on the CPU = %f \n", reference);
+    
+	gettimeofday(&start1,NULL);
+		double reference = compute_gold(a, b, n, h);
+    gettimeofday(&stop1,NULL);
+    
+	float time1 = (float) (stop1.tv_sec - start1.tv_sec +(stop1.tv_usec - start1.tv_usec) / (float) 1000000);
+  	printf("Reference solution computed on the CPU = %f \nRun time: %f\n", reference,time1);
 
 	/* Write this function to complete the trapezoidal on the GPU. */
-	double pthread_result = compute_using_pthreads(a, b, n, h);
-	printf("Solution computed using pthreads = %f \n", pthread_result);
+    gettimeofday(&start2,NULL);
+		double pthread_result = compute_using_pthreads(a, b, n, h);
+    gettimeofday(&stop2,NULL);
+    float time2 = (float) (stop2.tv_sec - start2.tv_sec +(stop2.tv_usec - start2.tv_usec) / (float) 1000000);
+	
+	printf("Solution computed using pthreads = %f \nRun time: %f\n", pthread_result,time2);
+    printf("Speedup: %f times\n",time1/time2);
+
+//	double reference = compute_gold(a, b, n, h);
+  // printf("Reference solution computed on the CPU = %f \n", reference);
+
+	/* Write this function to complete the trapezoidal on the GPU. */
+//	double pthread_result = compute_using_pthreads(a, b, n, h);
+//	printf("Solution computed using pthreads = %f \n", pthread_result);
 } 
 
 
@@ -89,44 +108,56 @@ double compute_gold(float a, float b, int n, float h) {
 double compute_using_pthreads(float a, float b, int n, float h)
 {
 	int i;
-	pthread_t* thread_handles;
+	int rv;
+	int thread_count = NUM_THREADS;
+	pthread_t* pthreads;
 	
-	local_n = n / NUM_THREADS;
-	thread_handles = malloc (NUM_THREADS*sizeof(pthread_t));
+	thread_n = n / thread_count;
+	pthreads = malloc (thread_count*sizeof(pthread_t));
 	
 	pthread_mutex_init(&mutex, NULL);
 	
 	for(i = 0; i < NUM_THREADS; i++)
 	{
-		pthread_create(&thread_handles[i], NULL, pthread_compute, (void*) i);
+		rv = pthread_create(&pthreads[i], NULL, pthread_compute, (void*) i);
+		if(rv)
+		{
+			printf("Error creating Threads \n");
+		}
+		
 	}
 	
 	for(i = 0; i < NUM_THREADS; i++)
 	{
-		pthread_join(thread_handles[i], NULL);
+		rv = pthread_join(pthreads[i], NULL);
+		if(rv)
+		{
+			printf("Error joining Threads \n");
+		}
 	}
 
 	pthread_mutex_destroy(&mutex);
-	free(thread_handles);
+	free(pthreads);
 
 	return area;
 }
 
 void pthread_compute(void* rank)
 {
-	double local_a;
-	double local_b;
-	double my_int;
-	long my_rank = (long) rank;
+	double thread_a;
+	double thread_b;
+	double thread_area;
 	double h;
+	long thread_num = (long) rank;
 	
-	h = (RIGHT_ENDPOINT - LEFT_ENDPOINT)/(float) NUM_TRAPEZOIDS;
-	local_a = LEFT_ENDPOINT + my_rank*local_n*h;
-	local_b = local_a + local_n*h;
-
-	my_int = compute_gold(local_a, local_b, local_n, h);
+	h = (RIGHT_ENDPOINT - LEFT_ENDPOINT) / (float) NUM_TRAPEZOIDS;
+	
+	thread_a = LEFT_ENDPOINT + thread_num * thread_n * h;
+	thread_b = thread_a + thread_n * h;
+	thread_area = compute_gold(thread_a, thread_b, thread_n, h);
+	
 	pthread_mutex_lock(&mutex);
-	area += my_int;
+	area += thread_area;
 	pthread_mutex_unlock(&mutex);
 
 }
